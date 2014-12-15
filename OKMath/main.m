fc30d5d7d9a889a89de6ccfc457b1f298020c85b
omer <omer@Mac-MacBook-Pro.local>
2014-12-15 17:29:20 +0200

    Initial Commit

---
+++ OKMath/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OKMath
+//
+//  Created by omer on 15.12.2014.
+//  Copyright (c) 2014 omer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
